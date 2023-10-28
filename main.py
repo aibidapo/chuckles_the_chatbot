1d7e53a0dc6d81c96886224a4febd29cd5380d94
aibidapo <24958100+aibidapo@users.noreply.github.com>
2023-10-28 06:44:25 -0500

    integrate chatbot with FastAPI

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import openai
 from dotenv import dotenv_values
+from fastapi import FastAPI, Form
+from typing import Annotated
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
 
-chat_log = []
+app = FastAPI()
 
-while True:
-    
-    user_input = input()
-    if user_input.casefold() == 'stop':
-        break
-    
+chat_log = [{
+    'role': 'system',
+    'content': 'You are a Python tutor AI, completely dedicated to teaching users how to learn Python programming from scratch. please provide clear instructions\
+        on Python concepts, best practices and syntax. Help create a path of learning for users to be able to create "real life, production-ready python applications".'
+}]
+
+@app.post("/")
+async def chat(user_input: Annotated[str, Form()]):
+        
     chat_log.append({'role': 'user', 'content': user_input})
-    
-    
-
     response = openai.ChatCompletion.create(
         model = 'gpt-3.5-turbo',
         messages = chat_log,
@@ -27,4 +29,4 @@ while True:
     bot_reponse = response['choices'][0]['message']['content']
     chat_log.append({'role': 'assistant', 'content': bot_reponse})
 
-    print(bot_reponse)
+    return bot_reponse
