54d10b05729dba72f56c5f59fa4577231b87d3f0
aibidapo <24958100+aibidapo@users.noreply.github.com>
2023-10-28 06:07:05 -0500

    create chatbot log for continuous communication.

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import openai
-
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-
 openai.api_key = config["OPENAI_API_KEY"]
 
 
-response = openai.ChatCompletion.create(
-    model = 'gpt-3.5-turbo',
-    messages = [{
-        'role': 'system',
-        'content': 'You are a helpful assistant'
-    }]
-)
+chat_log = []
 
-print(response)
+while True:
+    
+    user_input = input()
+    if user_input.casefold() == 'stop':
+        break
+    
+    chat_log.append({'role': 'user', 'content': user_input})
+    
+    
+
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        messages = chat_log,
+        
+        temperature = 0.7
+    )
+    
+    bot_reponse = response['choices'][0]['message']['content']
+    chat_log.append({'role': 'assistant', 'content': bot_reponse})
+
+    print(bot_reponse)
